32b190e58c6c708476bc777c731565071bf4dce0
CapUlDis <capuldis@gmail.com>
2021-02-09 17:48:35 +0300

    Test for creating categories route.

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -18,4 +18,19 @@ describe('User API', () => {
         expect(res.statusCode).toEqual(201);
         expect(res.body).toHaveProperty('group');
     })
+
+    it('should create new categories', async () => {
+        const res = await request(app)
+            .post('/api/categories')
+            .send({
+                categories: [
+                    { title: 'Завтрак', index: 0, groupId: 800 },
+                    { title: 'Супы', index: 1, groupId: 800 },
+                    { title: 'Пицца', index: 2, groupId: 800 },
+                    { title: 'Паста', index: 3, groupId: 800 }
+                ]
+            });
+        expect(res.statusCode).toEqual(201);
+        expect(res.body).toHaveProperty('categories');
+    })
 });
