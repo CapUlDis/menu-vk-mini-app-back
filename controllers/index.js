a46cddbeac334e335d884706c5a2e78fd0ae5ba3
CapUlDis <capuldis@gmail.com>
2021-02-09 14:34:51 +0300

    Add transaction.

--- controllers/index.js
+++ controllers/index.js
@@ -1,18 +1,17 @@
 const { Group } = require('../models');
+const db = require('../models');
 
-const createGroup = async (req, res) => {
-    try {
+const createGroup = (req, res) => {
+    db.sequelize.transaction(async t => {
         const group = await Group.create(req.body);
-        return res.status(201).json({
-            group,
-        });
-    } catch (error) {
+        return res.status(201).json({ group });
+    }).catch((error) => {
         return res.status(500).json({ error: error.message });
-    }
+    });
 };
 
-const getGroupMenuById = async (req, res) => {
-    try {
+const getGroupMenuById = (req, res) => {
+    db.sequelize.transaction(async t => {
         const { groupId } = req.params;
         const group = await Group.findOne({
             where: { groupId: groupId },
@@ -25,9 +24,9 @@ const getGroupMenuById = async (req, res) => {
             return res.status(200).json({ group });
         }
         return res.status(404).send('Group with the specified ID does not exists');
-    } catch (error) {
+    }).catch((error) => {
         return res.status(500).send(error.message);
-    }
+    });
 };
 
 module.exports = {
--- models/index.js
+++ models/index.js
@@ -2,11 +2,15 @@
 
 const fs = require('fs');
 const path = require('path');
+const cls = require('cls-hooked');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
+
+const namespace = cls.createNamespace('sequelize');
+Sequelize.useCLS(namespace);
 
 let sequelize;
 if (config.use_env_variable) {
