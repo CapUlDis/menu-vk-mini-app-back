b3fb0f0cdeede707adc4974ed5b6172344f36ad8
CapUlDis <capuldis@gmail.com>
2021-04-05 18:06:10 +0300

    Add read arguments http and migrate from shell to run server and migration, move sequelize-cli to prod.

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const bearerToken = require('express-bearer-token');
 const logger = require('morgan');
+const { exec } = require('child_process');
 
 const routes = require('./routes');
 
 const PORT = process.env.PORT || 3000;
+
+const args = process.argv.slice(2);
 
 const app = express();
 app.use(cors());
@@ -15,6 +18,24 @@ app.use(logger('dev'));
 
 app.use('/api', routes);
 
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+switch(args[0]) {
+  case 'http':
+    app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+    break;
+  case 'migrate':
+    new Promise((resolve, reject) => {
+      const migrate = exec(
+        'npx sequelize-cli db:migrate',
+        { env: process.env },
+        err => (err ? reject(err): resolve())
+      );
+    
+      // Forward stdout+stderr to this process
+      migrate.stdout.pipe(process.stdout);
+      migrate.stderr.pipe(process.stderr);
+    });
+    break;
+}
+
 
 module.exports = app;
